fd3b13c9863a0bae65c92691fc8a2056e767f060
Eduardo Estevão <eduardo.estevao.ext@pdg.com.br>
2018-09-10 19:32:08 -0300

    primeiro commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+
+        {
+            var movie = new Movie() { Name = "Shreak"};
+
+            //return View(movie);
+            //return Content("Helooo");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+
+            
+        }
+    }
+}
